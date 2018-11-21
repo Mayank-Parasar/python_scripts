835bd3cd10b2736b109fec87a84ef8dec9bbc571
Mayank Parasar <mayankparasar@gmail.com>
2018-11-21 16:13:22 -0500

    mods to script

--- launch_sim.py
+++ launch_sim.py
@@ -18,6 +18,7 @@ injection_rate = 0.02
 # while(packet_latency < 100 ):
 os.system("./build/Garnet_standalone/gem5.opt configs/example/garnet_synth_traffic.py --topology=Mesh_XY --num-cpus=64 --num-dirs=64 --mesh-rows=8 --network=garnet2.0 --router-latency=1  --sim-cycles=10000  --inj-vnet=0 --vcs-per-vnet=4 --injectionrate=%f --synthetic=uniform_random --routing-algorithm=0"%(injection_rate))
 packet_latency = subprocess.check_output("grep average_flit_latency m5out/stats.txt | sed 's/system.ruby.network.average_flit_latency\s*//'", shell=True)
+pkt_lat = float(packet_latency)
 # print ("Packet Latency: %f"%((float)packet_latency))
-print ("Packet Latency: %s" %(packet_latency))
+print ("Packet Latency: %f" %(pkt_lat))
 print ("Hello world")
